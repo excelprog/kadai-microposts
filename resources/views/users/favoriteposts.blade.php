e8c66b7f0396539877d5e19332e1e8bd2ad2cc9d
y.m <sunny_yoko45@yahoo.co.jp>
2018-11-30 13:39:14 +0000

    modify username

--- resources/views/users/favoriteposts.blade.php
+++ resources/views/users/favoriteposts.blade.php
@@ -1,6 +1,7 @@
 @if (count($microposts) > 0)
 <ul class="media-list">
 @foreach ($microposts as $micropost)
+    <?php $user = $micropost->user; ?>
     <li class="media">
         <div class="media-left">
             <img class="media-object img-rounded" src="{{ Gravatar::src($user->email, 50) }}" alt="">
